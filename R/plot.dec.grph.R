abcb047810890bdc44296bdc268ca67eb62671d4
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-07 14:56:54 +0200

    Add files via upload

---
+++ R/plot.dec.grph.R
@@ -0,0 +1,87 @@
+plot.dec.grph <- function(nds.df,
+                          eds.df,
+                          doss,site,decor,
+                          shw=c("nodes"),
+                          nd.color='orange',
+                          nd.size=1.7,
+                          lbl.txt='id',
+                          lbl.color='orange',
+                          lbl.size=1.2,
+                          ed.color='orange',
+                          ed.lwd=4){
+  ## plot nodes on image
+  # lbl.txt = 'id' ; nd.color <- lbl.color <- ed.color <- 'orange'
+  # lbl.size=1.2 ; nd.size=1.7 ; ed.lwd <- 4
+  # site <- "Cerro Muriano" ; decor <- "Cerro Muriano 1" ; 
+  # shw <- "edges" ; shw <- c("nodes","edges")
+  ## nodes
+  # select
+  nds.df.select <- nds.df[nds.df[,"site"]==site & nds.df[,"decor"]==decor,]
+  ## image
+  imgs <- read.table(file = paste0(doss,"/imgs.tsv"), sep = '\t', header = TRUE)
+  # select
+  img.select <- imgs[imgs[,"site"]==site & imgs[,"decor"]==decor,]
+  img.select <- img.select$img
+  # load
+  img.in <- magick::image_read(paste0(doss,'/',img.select))
+  img.out <- magick::image_draw(img.in)
+  print(shw)
+  if ("edges" %in% shw){
+    for (edg in 1:nrow(eds.df)){
+      # edg <- 1
+      # type of edge
+      ed.type <- ifelse(is.na(eds.df[edg,"type"]),1,
+                        ifelse(eds.df[edg,"type"]=='+',2,1)
+      )
+      lines(c(eds.df[edg,"xa"],eds.df[edg,"xb"]),
+            c(abs(eds.df[edg,"ya"]),abs(eds.df[edg,"yb"])),
+            lty=ed.type,
+            lwd=ed.lwd,col=ed.color)
+    }
+  }
+  if ("nodes" %in% shw){
+    ## loop through nodes to plot them and labels
+    for (n in 1:nrow(nds.df.select)){
+      # n <- 1
+      ax <- nds.df.select[n,"x"] ; ay <- abs(nds.df.select[n,"y"]) # coordinates
+      lbl <- nds.df.select[n,lbl.txt] # labels
+      points(ax,ay,
+             pch=21,
+             col=nd.color,
+             bg=nd.color,
+             cex=nd.size)
+      # labels shadow
+      # labels.shadow <- function(x, y=NULL, labels, col='white', bg='black', 
+      #                           theta= seq(0, 2*pi, length.out=50), r=0.1, ... ) {
+      # TODO: credits
+      # drop shadow
+      # xy <- xy.coords(ax,ay)
+      # r=0.2
+      # xo <- r*strwidth('A')
+      # yo <- r*strheight('A')
+      # # draw background text with small shift in x and y in background colour
+      # theta= seq(0, 2*pi, length.out=50)
+      # for (i in theta) {
+      #   text(xy$x+cos(i)*xo, xy$y+sin(i)*yo,
+      #        lbl, col=lbl.color, bg="white", cex=lbl.size, pos=3)
+      # }
+      labels.shadow(ax,ay,
+                    label=lbl,
+                    col=lbl.color,
+                    bg="white",
+                    cex=lbl.size,
+                    r=0.2,
+                    pos=3)
+    }
+  }
+  ## annotate
+  # studied variable up
+  img.out <- image_annotate(img.out, lbl.txt, size = 30,
+                            gravity = "northwest", color = "black")
+  # decor title down
+  tit.img <- paste0(site,"\n",decor)
+  img.out <- image_annotate(img.out, tit.img, size = 30,
+                            gravity = "southwest", color = "black")
+  # img.out <- f.annotate(img.out,site,decor)
+  return(img.out)
+}
---
+++ R/read.eds.R
@@ -0,0 +1,60 @@
+read.eds <- function(doss,site,decor,edges,nodes="nodes",dev=".tsv"){
+  # doss <- doss.extdata ; edges <- "edges" ; dev <- ".shp"
+  # doss <- doss.extdata ; edges <- "edges" ; dev <- ".tsv"
+  # nodes <- "nodes"
+  # site <- "Cerro Muriano" ; decor <- "Cerro Muriano 1"
+  # read nodes from .tsv or shapefile
+  # TODO: read .csv
+  eds.shp.coords <- data.frame(xa=numeric(0),
+                               ya=numeric(0),
+                               xb=numeric(0),
+                               yb=numeric(0))
+  if(dev==".tsv"){
+    # read nodes to get coordinates
+    flag.coords <- F
+    if(file.exists(paste0(doss,'/',nodes,".tsv"))){
+      nds.df <- read.nds(doss,nodes,".tsv")
+      flag.coords <- T
+    }
+    if(file.exists(paste0(doss,'/',nodes,".shp"))){
+      nds.df <- read.nds(doss.extdata,"nodes",".shp")
+      flag.coords <- T
+    }
+    if(!flag.coords){
+      return(paste0("Error: coordinates of the edges cannot be calculated",
+                    "\ncreate first a nodes dataframe or a nodes shapefile",
+                    "\nor create a shapefile for the edges"))
+    }
+    eds.df <- read.table(file = paste0(doss,"/",edges,dev), sep = '\t', header = TRUE)
+    eds.df <- eds.df[eds.df[,"site"]==site & eds.df[,"decor"]==decor,]
+    # get coordinates from nodes
+    for (a.edge in 1:nrow(eds.df)){
+      # a.edge <- 1
+      nd.a <- eds.df[a.edge,"a"]
+      nd.b <- eds.df[a.edge,"b"]
+      xa <- nds.df[nds.df[,"id"]==nd.a,"x"]
+      ya <- nds.df[nds.df[,"id"]==nd.a,"y"]
+      xb <- nds.df[nds.df[,"id"]==nd.b,"x"]
+      yb <- nds.df[nds.df[,"id"]==nd.b,"y"]
+      eds.shp.coords[nrow(eds.shp.coords)+1,] <- c(xa,ya,xb,yb)
+    }
+    eds.df <- cbind(eds.df,eds.shp.coords)
+  }
+  if(dev==".shp"){
+    eds.shp <- rgdal::readOGR(dsn = doss, layer = edges)
+    # extract edges coordinates
+    coords.eds <- lapply(slot(eds.shp, "lines"), function(x) lapply(slot(x, "Lines"),
+                                                                    function(y) slot(y, "coords")))
+    # loop to get coordinates and fill df
+    for (a.edge in 1:length(coords.eds)){
+      # a.edge <- 1
+      a.df <- coords.eds[[a.edge]][[1]]
+      xa <- a.df[1,1];ya <- a.df[1,2];xb <- a.df[2,1];yb <- a.df[2,2];
+      eds.shp.coords[nrow(eds.shp.coords)+1,] <- c(xa,ya,xb,yb)
+      # coords.df <- as.data.frame(rbind(coords.df,c(xa,ya,xb,yb)))
+    }
+    eds.df <- eds.shp@data
+    eds.df <- cbind(eds.df,eds.shp.coords)
+  }
+  return(eds.df)
+}
