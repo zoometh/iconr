63557f94a7eb096f0a956dbae92142ceb225beb0
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-07 15:59:35 +0200

    Update read.nds.R

--- R/read.nds.R
+++ R/read.nds.R
@@ -1,10 +1,11 @@
-read.nds <- function(doss,site,decor,dev=".tsv"){
+read.nds <- function(doss,site,decor,nodes,dev=".tsv"){
   # doss <- doss.extdata ; nodes <- "nodes" ; dev <- ".shp"
   # doss <- doss.extdata ; nodes <- "nodes" ; dev <- ".tsv"
   # read nodes from .tsv or shapefile
   # TODO: read .csv
   if(dev==".tsv"){
     nds.df <- read.table(file = paste0(doss,"/",nodes,dev), sep = '\t', header = TRUE)
+    nds.df <- nds.df[nds.df[,"site"]==site & nds.df[,"decor"]==decor,]
   }
   if(dev==".shp"){
     nds.shp <- rgdal::readOGR(dsn = doss, layer = nodes)
@@ -12,6 +13,7 @@ read.nds <- function(doss,site,decor,dev=".tsv"){
     colnames(nds.shp.coords) <- c("x","y")
     nds.df <- nds.shp@data
     nds.df <- cbind(nds.df,nds.shp.coords)
+    nds.df <- nds.df[nds.df[,"site"]==site & nds.df[,"decor"]==decor,]
   }
   return(nds.df)
 }
