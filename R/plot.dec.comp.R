b87298d794b54c0e6dfe0a3f42efd3ced86a3d56
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-17 22:05:54 +0200

    Create plot.dec.comp.R

---
+++ R/plot.dec.comp.R
@@ -0,0 +1,18 @@
+plot.dec.comp <- function(listg,graph2){
+  # a manner to use only plot()
+  # listg <- g.compar ; graph2 <- c(1,3)
+  # get row index in tcompar
+  lidf <- unlist(lapply(lgrph, function(x) x$name))
+  ldec.comp <- t(combn(lidf, 2)) # all pairwise comparisons
+  A <- graph2[1] ; B <- graph2[2] # ; nb.comm.eds <-
+  ridx <- which(df.compar[,1] == A & df.compar[,2] == B, arr.ind = T)
+  g <- listg[[ridx]]
+  out.compar <- paste0("compar_",as.character(graph2[1]),"_",as.character(B),".png")
+  png(out.compar,width = 14,height=7, units = "cm", res=300)
+  par(mfrow=c(1,2),
+      mar=c(0,0,0,0))    # set the plotting area into a 1*2 array
+  side.plot(g,1);side.plot(g,2) # call to plot
+  mtext(tit, side = 1, line = -1, outer = TRUE)
+  dev.off()
+  shell.exec(out.compar)
+}
