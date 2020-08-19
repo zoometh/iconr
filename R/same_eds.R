00c8c7f2a2d1e22ca28f457f759e4a875b8c0393
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-19 12:18:36 +0200

    Update same_eds.R

--- R/same_eds.R
+++ R/same_eds.R
@@ -6,15 +6,7 @@ same_eds <- function(imgs,nodes,edges,var="type"){
                            ncol = length(lgrph))
   mat.same_edges <- as.data.frame(mat.same_edges)
   rownames(mat.same_edges) <- colnames(mat.same_edges)<- lgrph_nmes
-  mat.same_edges[is.na(mat.same_edges)] <- 0 # replace NA with 0
-  # lgrph_nmes <- c()
-  # for (i in 1:length(lgrph)){
-  #   lgrph_nmes <- c(lgrph_nmes,lgrph[[i]]$name)
-  #   print (lgrph[[i]]$name)
-  # }
-  # identify same edges btw graphs
-  # return all edges as a vector character
-  
+  mat.same_edges[is.na(mat.same_edges)] <- 0 # replace NA with 0  
   # list all edges
   tot.edges <- data.frame("V1"=character(),
                           "V2"=character(),
@@ -24,12 +16,8 @@ same_eds <- function(imgs,nodes,edges,var="type"){
   for(i in 1:length(lgrph)){
     # i <- 1
     a.g <- lgrph[[i]]
-    #print(paste0(i,".",a.g$name))
     g.edg <- as.data.frame(igraph::as_edgelist(a.g)) # edges
     g.edg$type <- igraph::edge.attributes(a.g)$type # 
-    # if (rm.attributes){
-    #   g.edg <- g.edg[g.edg$typ != "+",]
-    # }
     tot.edges <- rbind(tot.edges,g.edg)
   }
   #tot.edges$typ[is.na(tot.edges$typ)] <- "=" # change NA to =
@@ -37,7 +25,7 @@ same_eds <- function(imgs,nodes,edges,var="type"){
   tot.edges <- tot.edges[!duplicated(t(apply(tot.edges[c("V1", "V2","type")], 1, sort))), ]
   sel.edges <- paste0(tot.edges[,1],tot.edges[,"type"],tot.edges[,2]) # like "nez=vide"
   # sel.edges <- f.tot.edges(lgrph)
-  df.typ_edges <- f.typ.edges(sel.edges,lgrph) # call function
+  df.typ_edges <- f_typ_edges(sel.edges,lgrph) # call function
   # at least a common edge appears 2 times, limit the df
   df.typ_edges.sub <- df.typ_edges[,colSums(df.typ_edges) > 1]
   for (c in 1:ncol(df.typ_edges.sub)){
