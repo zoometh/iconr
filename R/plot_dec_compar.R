3d23147ed12149bcabc4c2c089fa461b4c49a02b
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-19 12:16:44 +0200

    Update plot_dec_compar.R

--- R/plot_dec_compar.R
+++ R/plot_dec_compar.R
@@ -2,7 +2,8 @@ plot_dec_compar <- function(listg,graph2){
   # a manner to use only plot()
   # listg <- g.compar ; graph2 <- c(1,3)
   # get row index in tcompar
-  lidf <- unlist(lapply(listg, function(x) x$name))
+  flistg <- unlist(listg, recursive = FALSE) # flatten list
+  lidf <- unique(unlist(lapply(flistg, function(x) x$name)))
   ldec.comp <- t(utils::combn(lidf, 2)) # all pairwise comparisons
   A <- graph2[1] ; B <- graph2[2] # ; nb.comm.eds <-
   ridx <- which(ldec.comp[,1] == A & ldec.comp[,2] == B, arr.ind = T)
@@ -11,8 +12,8 @@ plot_dec_compar <- function(listg,graph2){
   tit <- paste0("compare decorations '",A,"' and '",B,"'")
   grDevices::png(out.compar,width = 14,height=7, units = "cm", res=300)
   graphics::par(mfrow=c(1,2),
-      mar=c(0,0,0,0))    # set the plotting area into a 1*2 array
-  side.plot(g,1); side.plot(g,2) # call to plot
+                mar=c(0,0,0,0))    # set the plotting area into a 1*2 array
+  side_plot(g,1); side_plot(g,2) # call to plot
   graphics::mtext(tit, side = 1, line = -1, outer = TRUE)
   grDevices::dev.off()
   shell.exec(out.compar)
