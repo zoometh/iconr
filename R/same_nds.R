4e733063773f9bb3e9ea5a423105a17e508fb77d
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-19 10:58:42 +0200

    Update and rename same.nds.R to same_nds.R

--- R/same_nds.R
+++ R/same_nds.R
@@ -1,6 +1,6 @@
-same.nds <- function(imgs,nodes,edges,var="type"){
+same_nds <- function(imgs,nodes,edges,var="type"){
   # var = "type"
-  lgrph <- list.grph(imgs,nodes,edges,var) # call function
+  lgrph <- list_grph(imgs,nodes,edges,var) # call function
   lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name)) # get names
   lcomp <- as.data.frame(t(combn(lgrph_nmes,2)))
   mat.same_nodes <- matrix(nrow = length(lgrph),
