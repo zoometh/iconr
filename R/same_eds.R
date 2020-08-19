e2a8a70c5e56ebbb2f87c8e0622b8956019a1ed9
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-19 16:40:13 +0200

    Update same_eds.R

--- R/same_eds.R
+++ R/same_eds.R
@@ -1,5 +1,6 @@
 same_eds <- function(imgs,nodes,edges,var="type"){
   # TODO: change "type" to 'var'
+  # var <- "type"
   lgrph <- list_grph(imgs,nodes,edges,var) # call function
   lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name)) # get names
   mat.same_edges <- matrix(nrow = length(lgrph),
@@ -12,39 +13,37 @@ same_eds <- function(imgs,nodes,edges,var="type"){
                           "V2"=character(),
                           "type"=character(),
                           stringsAsFactors=FALSE)
-  # all unique type of edges
+  # all edges
   for(i in 1:length(lgrph)){
-    # i <- 1
+    # i <- 4
     a.g <- lgrph[[i]]
     g.edg <- as.data.frame(igraph::as_edgelist(a.g)) # edges
     g.edg$type <- igraph::edge.attributes(a.g)$type # 
     tot.edges <- rbind(tot.edges,g.edg)
   }
-  #tot.edges$typ[is.na(tot.edges$typ)] <- "=" # change NA to =
-  # opt suppress duplicated
-  tot.edges <- tot.edges[!duplicated(t(apply(tot.edges[c("V1", "V2","type")], 1, sort))), ]
-  sel.edges <- paste0(tot.edges[,1],tot.edges[,"type"],tot.edges[,2]) # like "nez=vide"
-  # sel.edges <- f.tot.edges(lgrph)
-  df.typ_edges <- f_typ_edges(sel.edges,lgrph) # call function
+  tot.edges <- data.frame(lapply(tot.edges, as.character),stringsAsFactors = F)
+  tot.edges$V1 <- paste0(tot.edges$V1,tot.edges$type)
+  tot.edges$V2 <- paste0(tot.edges$V2,tot.edges$type)
+  tot.edges <- tot.edges[!duplicated(t(apply(tot.edges[1:2], 1, sort))), ] # rm duplicated
+  df.typ_edges <- f_typ_edges(tot.edges,lgrph,verbose = FALSE) # call function
   # at least a common edge appears 2 times, limit the df
   df.typ_edges.sub <- df.typ_edges[,colSums(df.typ_edges) > 1]
-  for (c in 1:ncol(df.typ_edges.sub)){
-    #c <- 2
-    a.col <- df.typ_edges.sub[,c] # get column
-    # at least 2 decoration without 0
-    if(length(a.col[!a.col %in% 0]) > 1){
-      idxs <- which(a.col %in% c(1:100)) # count of egal betw 1 and Inf
-      all.comb <- subset(expand.grid(rep(list(idxs),2)), Var1 != Var2)
-      for (r in 1:nrow(all.comb)){
-        #r <- 1
-        x <- all.comb[r,"Var1"];y <- all.comb[r,"Var2"]
-        mat.same_edges[x,y] <- mat.same_edges[x,y]+1
-        mat.same_edges[y,x] <- mat.same_edges[y,x]+1
-      }
+  all.comp <- as.data.frame(t(utils::combn(row.names(df.typ_edges.sub),2)))
+  for (a.comp in 1:nrow(all.comp)){
+    # all pairwise comparisons
+    # a.comp <- 1
+    idx.A <- as.character(all.comp[a.comp,1])
+    idx.B <- as.character(all.comp[a.comp,2])
+    df.typ_edges.sub.ct <- df.typ_edges.sub[c(idx.A,idx.B),]
+    ct <- 0
+    for (c in 1:ncol(df.typ_edges.sub.ct)){
+      # c <- 1
+      # take the min
+      ct.col <- min(df.typ_edges.sub.ct[1,c],df.typ_edges.sub.ct[2,c])
+      ct <- ct+ct.col
     }
+    mat.same_edges[idx.A,idx.B] <- ct
+    mat.same_edges[idx.B,idx.A] <- ct
   }
-  # divide all cells by 2 (symetry)
-  mat.same_edges[] <- lapply(mat.same_edges, function(x) x/2)
-  colnames(mat.same_edges) <- row.names(mat.same_edges) <- lgrph_nmes
   return(mat.same_edges)
 }
