d0bb9bc3f0038900ceefce96a87c99b432dd93af
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-20 16:01:33 +0200

    Update same_eds.R

--- R/same_eds.R
+++ R/same_eds.R
@@ -1,6 +1,8 @@
 same_eds <- function(lgrph,var="type"){
   # TODO: change "type" to 'var'
+  # TODO: put 'verbose' in param
   # var <- "type"
+  verbose <- TRUE
   lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name)) # get names
   mat.same_edges <- matrix(nrow = length(lgrph),
                            ncol = length(lgrph))
@@ -24,7 +26,43 @@ same_eds <- function(lgrph,var="type"){
   tot.edges$V1 <- paste0(tot.edges$V1,tot.edges$type)
   tot.edges$V2 <- paste0(tot.edges$V2,tot.edges$type)
   tot.edges <- tot.edges[!duplicated(t(apply(tot.edges[1:2], 1, sort))), ] # rm duplicated
-  df.typ_edges <- f_typ_edges(tot.edges,lgrph,verbose = FALSE) # call private function
+                              sel.edges <- paste0(tot.edges[,1],tot.edges[,2])
+  # anct 'f_typ_edges' function - - - - - - - - - - - - - - - - - - - - - -
+  sel.edges <- gsub(".{1}$","",sel.edges) # rm last character
+  df.typ_edges <- as.data.frame(matrix(nrow = length(lgrph),
+                                       ncol = length(sel.edges)))
+  # rename col and row
+  lidf <- unlist(lapply(lgrph, function(x) x$name))
+  rownames(df.typ_edges) <-  lidf # get names
+  colnames(df.typ_edges) <- sel.edges
+  df.typ_edges[is.na(df.typ_edges)] <- 0 # default empty cells
+  for (i in 1:length(lgrph)){
+    # graph have same idf in the list and in dataframes
+    if (verbose){print(paste0("read graph: ",lgrph[[i]]$name))}
+    #mat <- as_edgelist(lgrph[[i]])
+    # i <- 4
+    mat <- as.data.frame(igraph::as_edgelist(lgrph[[i]])) # edges
+    mat$type <- igraph::edge.attributes(lgrph[[i]])$type # typ of edges
+    # mat$type[is.na(mat$type)] <- "=" # change NA to =
+    for (j in 1:nrow(mat)){
+      #j <- 12
+      AB <- paste0(mat[j,1],mat[j,"type"],mat[j,2])
+      BA <- paste0(mat[j,2],mat[j,"type"],mat[j,1])
+      if (verbose){print(paste0("      ",AB,' <=> ',BA))}
+      # undirected graphs
+      # fill with "nez=vide" or "vide=nez"
+      if (AB %in% colnames(df.typ_edges)){
+        df.typ_edges[i,AB] <- df.typ_edges[i,AB]+1 # add +1
+        if (verbose){print("                       match!")}
+      }
+      if (BA %in% colnames(df.typ_edges)){
+        df.typ_edges[i,BA] <- df.typ_edges[i,BA]+1 # add +1
+        if (verbose){print("                       match!")}
+      }
+    }
+  }
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # df.typ_edges <- f_typ_edges(tot.edges,lgrph,verbose = FALSE) # call private function
   # at least a common edge appears 2 times, limit the df
   df.typ_edges.sub <- df.typ_edges[,colSums(df.typ_edges) > 1]
   all.comp <- as.data.frame(t(utils::combn(row.names(df.typ_edges.sub),2)))
