0c9fad8bdf2f7b06d9b06cc7257bf1a64c8c9cd5
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-24 12:45:23 +0200

    Update label_shadow.R

--- R/label_shadow.R
+++ R/label_shadow.R
@@ -1,16 +1,17 @@
-labels_shadow <- function(x, y=NULL, labels, col='white', bg='black',
-                          theta= seq(0, 2*pi, length.out=50), r=0.1, ...) {
+labels_shadow <- function (x, y, labels, col, bg = "white", 
+          theta = seq(0, 2 * pi, length.out = 50), r = 0.1, ...) 
+{
   # TODO: credits
-  # drop shadow
-  xy <- grDevices::xy.coords(x,y)
-  r=0.1
-  xo <- r*graphics::strwidth('A')
-  yo <- r*graphics::strheight('A')
-  # draw background text with small shift in x and y in background colour
-  theta= seq(0, 2*pi, length.out=50)
+  xy <- grDevices::xy.coords(x, y)
+  r = 0.1
+  xo <- r * graphics::strwidth("A")
+  yo <- r * graphics::strheight("A")
+  theta = seq(0, 2 * pi, length.out = 50)
+  # background
   for (i in theta) {
-    graphics::text(xy$x + cos(i)*xo, xy$y + sin(i)*yo, labels,col='black', ... )
+    graphics::text(xy$x + cos(i) * xo, xy$y + sin(i) * yo, 
+                   labels, col = "white", ...)
   }
-  # draw actual text in exact xy position in foreground colour
-  graphics::text(xy$x, xy$y, labels, col='white', ... )
+  # foreground
+  graphics::text(xy$x, xy$y, labels, col = "orange", ...)
 }
