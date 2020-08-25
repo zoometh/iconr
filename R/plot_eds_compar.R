0e1b370abcb64a5746203014fb2b44151fd1ee2a
zoometh <67924910+zoometh@users.noreply.github.com>
2020-08-25 11:46:04 +0200

    Update plot_eds_compar.R

--- R/plot_eds_compar.R
+++ R/plot_eds_compar.R
@@ -1,6 +1,6 @@
-plot_eds_compar <- function(listg,graph2){
+plot_eds_compar <- function(listg,graph2,var="type"){
   # a manner to use only plot()
-  # listg <- g.compar ; graph2 <- c(1,3)
+  # listg <- g.compar ; graph2 <- c(1,4)
   # get row index in tcompar
   flistg <- unlist(listg, recursive = FALSE) # flatten list
   lidf <- unique(unlist(lapply(flistg, function(x) x$name)))
@@ -9,12 +9,12 @@ plot_eds_compar <- function(listg,graph2){
   ridx <- which(ldec.comp[,1] == A & ldec.comp[,2] == B, arr.ind = T)
   g <- listg[[ridx]]
   out.compar <- paste0("compar_eds_",as.character(A),"_",as.character(B),".png")
-                               tit <- paste0("compare decorations '",A,"' and '",B,"'")
+  tit <- paste0("compare decorations '",A,"' and '",B,"' on '",var,"'")
   grDevices::png(out.compar,width = 14,height=7, units = "cm", res=300)
   graphics::par(mfrow=c(1,2),
                 mar=c(0,0,0,0))    # set the plotting area into a 1*2 array
-  side_plot_eds(g,1); side_plot_eds(g,2) # call to plot
-  graphics::mtext(tit, side = 1, line = -1, outer = TRUE)
+  side_plot_eds(g,1,var); side_plot_eds(g,2,var) # call to plot
+  graphics::mtext(tit, side = 1, line = -1, outer = TRUE, cex=0.8)
   grDevices::dev.off()
   # shell.exec(out.compar)
 }
