aa0896bc635ffc7daa0085274a1702a1e57cb464
zoometh <67924910+zoometh@users.noreply.github.com>
2020-09-23 18:11:46 +0200

    Update plot_nds_compar.R

--- R/plot_nds_compar.R
+++ R/plot_nds_compar.R
@@ -1,8 +1,4 @@
 plot_nds_compar <- function(listg, graph2, doss=getwd(), var="type"){
-  # TODO: plot the field on wich the comparison is done one each node
-  # a manner to use only plot()
-  # listg <- g.compar ; graph2 <- c(1,4)
-  # get row index in tcompar
   flistg <- unlist(listg, recursive = FALSE) # flatten list
   lidf <- unique(unlist(lapply(flistg, function(x) x$name)))
   ldec.comp <- t(utils::combn(lidf, 2)) # all pairwise comparisons
@@ -11,7 +7,7 @@ plot_nds_compar <- function(listg, graph2, doss=getwd(), var="type"){
   g <- listg[[ridx]] # a row = two graphs
   out.compar <- paste0("compar_nds_",as.character(A),"_",as.character(B),".png")
   tit <- paste0("compare nodes of decorations '",A,"' and '",B,"'")
-  grDevices::png(out.compar,width = 14,height=7, units = "cm", res=300)
+  grDevices::png(out.compar, width = 14, height=7, units = "cm", res=300)
   graphics::par(mfrow=c(1,2),
                 mar=c(0,0,0,0))    # set the plotting area into a 1*2 array
   side_plot_nds(g, 1, doss, var); side_plot_nds(g, 2, doss, var) # call to plot
