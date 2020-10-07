ba5040d51115e0f8ed165d725a5637b1bd29d94c
zoometh <67924910+zoometh@users.noreply.github.com>
2020-10-07 15:27:13 +0200

    Update read_nds.R

--- R/read_nds.R
+++ R/read_nds.R
@@ -1,19 +1,21 @@
-read_nds <- function(site,decor,doss=getwd(),nodes="nodes",dev=".tsv"){
-  # doss <- doss.extdata ; nodes <- "nodes" ; dev <- ".shp"
-  # doss <- doss.extdata ; nodes <- "nodes" ; dev <- ".tsv"
-  # read nodes from .tsv or shapefile
-  # TODO: read .csv
-  if(dev==".tsv"){
-    nds.df <- utils::read.table(file = paste0(doss,"/",nodes,dev), sep = '\t', header = TRUE)
-    nds.df <- nds.df[nds.df[,"site"]==site & nds.df[,"decor"]==decor,]
-  }
-  if(dev==".shp"){
-    nds.shp <- rgdal::readOGR(dsn = doss, layer = nodes, verbose=F)
-    nds.shp.coords <- nds.shp@coords
-    colnames(nds.shp.coords) <- c("x","y")
-    nds.df <- nds.shp@data
-    nds.df <- cbind(nds.df,nds.shp.coords)
-    nds.df <- nds.df[nds.df[,"site"]==site & nds.df[,"decor"]==decor,]
-  }
-  return(nds.df)
+read_nds <- function(site, decor, doss = getwd(), nodes = "nodes", dev = ".tsv") {
+    # doss <- doss.extdata ; nodes <- 'nodes' ; dev <- '.shp' doss <-
+    # doss.extdata ; nodes <- 'nodes' ; dev <- '.tsv' read nodes from .tsv
+    # or shapefile TODO: read .csv
+    if (dev == ".tsv") {
+        nds.df <- utils::read.table(file = paste0(doss, "/", nodes, dev), 
+            sep = "\t", header = TRUE)
+        nds.df <- nds.df[nds.df[, "site"] == site & nds.df[, "decor"] == 
+            decor, ]
+    }
+    if (dev == ".shp") {
+        nds.shp <- rgdal::readOGR(dsn = doss, layer = nodes, verbose = F)
+        nds.shp.coords <- nds.shp@coords
+        colnames(nds.shp.coords) <- c("x", "y")
+        nds.df <- nds.shp@data
+        nds.df <- cbind(nds.df, nds.shp.coords)
+        nds.df <- nds.df[nds.df[, "site"] == site & nds.df[, "decor"] == 
+            decor, ]
+    }
+    return(nds.df)
 }
