59a0aaf075552b95d9fb2310532b609e0d582ca2
zoometh <67924910+zoometh@users.noreply.github.com>
2020-10-07 15:29:27 +0200

    Update same_nds.R

--- R/same_nds.R
+++ R/same_nds.R
@@ -1,5 +1,4 @@
 same_nds <- function(lgrph, var = "type") {
-    # var = 'type'
     lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name))  # get names
     lcomp <- as.data.frame(t(utils::combn(lgrph_nmes, 2)))
     mat.same_nodes <- matrix(nrow = length(lgrph), ncol = length(lgrph))
