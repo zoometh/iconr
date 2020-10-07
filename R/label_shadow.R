05835ab54c68d47c3ca0260c5e2400cce884af29
zoometh <67924910+zoometh@users.noreply.github.com>
2020-10-07 14:36:24 +0200

    Update label_shadow.R

--- R/label_shadow.R
+++ R/label_shadow.R
@@ -1,18 +1,17 @@
-labels_shadow <- function (x, y = NULL, labels, col = "black", bg = "white", 
-          theta = seq(0, 2 * pi, length.out = 50), r = 0.1, ...) 
-{
-  # all credits of the function go to Greg Snow
-  # see https://rdrr.io/cran/TeachingDemos/man/shadowtext.html
-  xy <- grDevices::xy.coords(x, y)
-  r = 0.1
-  xo <- r * graphics::strwidth("A")
-  yo <- r * graphics::strheight("A")
-  theta = seq(0, 2 * pi, length.out = 50)
-  # background
-  for (i in theta) {
-    graphics::text(xy$x + cos(i) * xo, xy$y + sin(i) * yo, 
-                   labels, col = "white", ...)
-  }
-  # foreground
-  graphics::text(xy$x, xy$y, labels, col = col, ...)
+labels_shadow <- function(x, y = NULL, labels, col = "black", bg = "white", 
+    theta = seq(0, 2 * pi, length.out = 50), r = 0.1, ...) {
+    # all credits of the function go to Greg Snow see
+    # https://rdrr.io/cran/TeachingDemos/man/shadowtext.html
+    xy <- grDevices::xy.coords(x, y)
+    r = 0.1
+    xo <- r * graphics::strwidth("A")
+    yo <- r * graphics::strheight("A")
+    theta = seq(0, 2 * pi, length.out = 50)
+    # background
+    for (i in theta) {
+        graphics::text(xy$x + cos(i) * xo, xy$y + sin(i) * yo, labels, 
+            col = "white", ...)
+    }
+    # foreground
+    graphics::text(xy$x, xy$y, labels, col = col, ...)
 }
