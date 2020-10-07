68656b415b835584a6212cbc465eecc31676361a
zoometh <67924910+zoometh@users.noreply.github.com>
2020-10-07 14:37:33 +0200

    Update list_dec.R

--- R/list_dec.R
+++ R/list_dec.R
@@ -1,33 +1,34 @@
-list_dec <- function(imgs,
-                      nodes,
-                      edges,
-                      var="type"){
-  # 'var': field on which the comparison will be done
-  # create list of graphs
-  lgrph <- list()
-  for (r in 1:nrow(imgs)){
-    # r <- 2
-    a.enr <- imgs[r,]
-    # utils::globalVariables(names(a.enr))
-    a.site <- a.enr$site ; a.decor <- a.enr$decor ; a.idf <- a.enr$idf
-    g.nodes <- subset(nodes, site == a.site & decor == a.decor) # nodes
-    g.nodes <- subset(g.nodes, select=-c(site,decor))
-    g.edges <- subset(edges, site == a.site & decor == a.decor) # edges
-    g.edges <- subset(g.edges, select=-c(site,decor))
-    # create graph
-    g <- igraph::graph_from_data_frame(g.edges, directed = FALSE, vertices= g.nodes)
-    g.nodes.idf <- igraph::V(g)$name # get name of vertices
-    igraph::V(g)$name <- igraph::as_data_frame(g, what="vertices")[,var]
-    # V(g)$name <- V(g)$type # remplace les numeros par leur types # TODO: permit change
-    igraph::V(g)$idf <- g.nodes.idf
-    # attributes
-    g$name <- a.idf # graph name
-    g$site <- a.site
-    g$decor <- a.decor
-    g$label <- paste0(as.character(a.idf),'-',a.site,'.',a.decor)
-    g$img <- a.enr[,"img"]
-    # g$name <- paste0(as.character(r),"-",a.site,'.',a.decor) # name
-    lgrph[[length(lgrph)+1]] <- g
-  }
-  return(lgrph)
+list_dec <- function(imgs, nodes, edges, var = "type") {
+    # 'var': field on which the comparison will be done create list of
+    # graphs
+    lgrph <- list()
+    for (r in 1:nrow(imgs)) {
+        # r <- 2
+        a.enr <- imgs[r, ]
+        # utils::globalVariables(names(a.enr))
+        a.site <- a.enr$site
+        a.decor <- a.enr$decor
+        a.idf <- a.enr$idf
+        g.nodes <- subset(nodes, site == a.site & decor == a.decor)  # nodes
+        g.nodes <- subset(g.nodes, select = -c(site, decor))
+        g.edges <- subset(edges, site == a.site & decor == a.decor)  # edges
+        g.edges <- subset(g.edges, select = -c(site, decor))
+        # create graph
+        g <- igraph::graph_from_data_frame(g.edges, directed = FALSE, vertices = g.nodes)
+        g.nodes.idf <- igraph::V(g)$name  # get name of vertices
+        igraph::V(g)$name <- igraph::as_data_frame(g, what = "vertices")[, 
+            var]
+        # V(g)$name <- V(g)$type # remplace les numeros par leur types # TODO:
+        # permit change
+        igraph::V(g)$idf <- g.nodes.idf
+        # attributes
+        g$name <- a.idf  # graph name
+        g$site <- a.site
+        g$decor <- a.decor
+        g$label <- paste0(as.character(a.idf), "-", a.site, ".", a.decor)
+        g$img <- a.enr[, "img"]
+        # g$name <- paste0(as.character(r),'-',a.site,'.',a.decor) # name
+        lgrph[[length(lgrph) + 1]] <- g
+    }
+    return(lgrph)
 }
