ff3e413da4942ac57c25bd0f0747d01fdc4e3357
Shennan <thomashuet7@gmail.com>
2020-11-06 17:52:24 +0100

    new changes

--- R/plot_nds_compar.R
+++ R/plot_nds_compar.R
@@ -6,14 +6,14 @@ plot_nds_compar <- function(listg, graph2, doss = getwd(), var = "type") {
     B <- graph2[2]  # ; nb.comm.eds <-
     ridx <- which(ldec.comp[, 1] == A & ldec.comp[, 2] == B, arr.ind = T)
     g <- listg[[ridx]]  # a row = two graphs
-    out.compar <- paste0("compar_nds_", as.character(A), "_", as.character(B), 
+    out.compar <- paste0("compar_nds_", as.character(A), "_", as.character(B),
         ".png")
     tit <- paste0("nodes: compare decorations '", A, "' and '", B, "'")
     grDevices::png(out.compar, width = 14, height = 7, units = "cm", res = 300)
     graphics::par(mfrow = c(1, 2), mar = c(0, 0, 0, 0))  # set the plotting area into a 1*2 array
     side_plot_nds(g, 1, doss, var)
     side_plot_nds(g, 2, doss, var)  # call to plot
-    graphics::mtext(tit, side = 1, line = -1, outer = TRUE, cex = 0.8)
+    graphics::mtext(tit, side = 1, line = -1, outer = TRUE, cex = 0.6)
     grDevices::dev.off()
     return(paste0(getwd(), "/", out.compar))
 }
