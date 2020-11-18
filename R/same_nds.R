353512e6de5c0e8b68f8c2392019ac1f5406e5cb
josempozo <72624361+josempozo@users.noreply.github.com>
2020-11-18 19:06:14 +0100

    Update same_nds.R
    Large simplification by using lapply and outer functions. This avoids the for loop and makes code much more compact and clear.

--- R/same_nds.R
+++ R/same_nds.R
@@ -1,25 +1,11 @@
 same_nds <- function(lgrph, var = "type") {
     lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name))  # get names
-    lcomp <- as.data.frame(t(utils::combn(lgrph_nmes, 2)))
-    mat.same_nodes <- matrix(nrow = length(lgrph), ncol = length(lgrph))
-    mat.same_nodes <- as.data.frame(mat.same_nodes)
+    g.nds = lapply(lgrph,
+                   function(x) igraph::get.vertex.attribute(x, var,
+                                                        index = igraph::V(x)))
+    mat.same_nodes <- outer(g.nds, g.nds,
+                            Vectorize(function(x,y) length(intersect(x,y))))
+    diag(mat.same_nodes) <- 0
     rownames(mat.same_nodes) <- colnames(mat.same_nodes) <- lgrph_nmes
-    mat.same_nodes[is.na(mat.same_nodes)] <- 0  # replace NA with 0
-    for (g in 1:nrow(lcomp)) {
-        # pairwise comparisons g <- 2
-        idx.A <- lcomp[g, 1]
-        idx.B <- lcomp[g, 2]
-        # grphs
-        g.a <- lgrph[[idx.A]]
-        g.b <- lgrph[[idx.B]]
-        # nodes
-        nds.a <- igraph::get.vertex.attribute(g.a, var, index = igraph::V(g.a))
-        nds.b <- igraph::get.vertex.attribute(g.b, var, index = igraph::V(g.b))
-        # intersect
-        comm.nds <- intersect(nds.a, nds.b)
-        # write the value in the symetric matrix
-        mat.same_nodes[lcomp[g, 1], lcomp[g, 2]] <- length(comm.nds)
-        mat.same_nodes[lcomp[g, 2], lcomp[g, 1]] <- length(comm.nds)
-    }
     return(mat.same_nodes)
 }
