5e270c8a0e1805ddd30ac9a240bbd8a69137cb0a
Shennan <thomashuet7@gmail.com>
2020-11-23 20:15:50 +0100

    retry

--- R/read_nds_contemp.R
+++ R/read_nds_contemp.R
@@ -1,5 +1,7 @@
 read_nds_contemp <- function(nds.df, eds.df, selected.nd){
   # selected.nd <- 3
+  site <- unique(nds.df$site)
+  decor <- unique(nds.df$decor)
   nds.df$site <- nds.df$decor <- NULL
   eds.df$site <- eds.df$decor <- NULL
   g <- igraph::graph_from_data_frame(eds.df, vertices= nds.df, directed=FALSE)
@@ -16,6 +18,10 @@ read_nds_contemp <- function(nds.df, eds.df, selected.nd){
   nds.overlap <- as.numeric(which(g.member != selected.comp))
   # g <- delete.vertices(g, over.nds)
   nds.df.contemp <- nds.df[!(nds.df$id %in% nds.overlap),]
+  nds.df.contemp$site <- site
+  nds.df.contemp$decor <- decor
   eds.df.contemp <- eds.df[-eds.overlap,]
+  eds.df.contemp$site <- site
+  eds.df.contemp$decor <- decor
   return(list(nds.df.contemp,eds.df.contemp))
 }
