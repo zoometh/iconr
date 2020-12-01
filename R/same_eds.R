dbcb1baf3e89024bda084abb52b51de25b046a8d
josempozo <72624361+josempozo@users.noreply.github.com>
2020-12-01 17:06:53 +0100

    Delete same_eds.R

--- R/same_eds.R
+++
@@ -1,8 +0,0 @@
-same_eds <- function(lgrph, var = "type") {
-    lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name))  # get names
-    g.eds <- lapply(lgrph, function(x) named_edges(x, var))
-    mat.same_edges <- outer(g.eds, g.eds,
-                            Vectorize(function(x,y) length(intersect(x,y))))
-    rownames(mat.same_edges) <- colnames(mat.same_edges) <- lgrph_nmes
-    return(mat.same_edges)
-}
