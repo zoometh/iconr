3d89883af3f3a27ea43166d368763c4c7ab36019
zoometh <67924910+zoometh@users.noreply.github.com>
2020-12-01 11:12:51 +0100

    Update same_eds.R

--- R/same_eds.R
+++ R/same_eds.R
@@ -1,4 +1,4 @@
-same_nds <- function(lgrph, var = "type") {
+same_eds <- function(lgrph, var = "type") {
     lgrph_nmes <- unlist(lapply(lgrph, function(x) x$name))  # get names
     g.nds <- lapply(lgrph, function(x) igraph::vertex_attr(x, var))
     mat.same_nodes <- outer(g.nds, g.nds,
