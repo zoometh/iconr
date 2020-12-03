727ae300c287600805fae4c7ee780cc1523b6a9c
Shennan <thomashuet7@gmail.com>
2020-12-03 13:39:31 +0100

    select edges corrections

--- R/contemp_nds.R
+++ R/contemp_nds.R
@@ -1,4 +1,5 @@
 contemp_nds <- function(nds.df, eds.df, selected.nd){
+  # nds.df, eds.df, selected.nd
   # selected.nd <- 3
   nds.g.cols <- !(names(nds.df) %in% c("site", "decor"))
   eds.g.cols <- !(names(eds.df) %in% c("site", "decor"))
@@ -9,6 +10,5 @@ contemp_nds <- function(nds.df, eds.df, selected.nd){
   g <- igraph::delete.edges(g, eds.overlap)
   g.member <- igraph::components(g)$membership
   contemp.cases <- g.member == g.member[selected.nd]
-
-  return(list(nds.df[contemp.cases, ], eds.df[contemp.cases, ]))
+  return(list(nds.df[contemp.cases, ], eds.df[eds.df$type != '>', ]))
 }
