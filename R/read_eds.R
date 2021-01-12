07b3900e20db277f45259f66bf9375df7167cfd7
josempozo <72624361+josempozo@users.noreply.github.com>
2021-01-12 19:56:36 +0100

    Update read_eds.R
    Changed parameter name from "doss" to "dir". I think that it is a more standard name. Please, tell me if you don't agree. This change is in all functions and documentations.

--- R/read_eds.R
+++ R/read_eds.R
@@ -1,16 +1,16 @@
 #' @export
-read_eds <- function(site, decor, doss = getwd(),
+read_eds <- function(site, decor, dir = getwd(),
                      edges = "edges", nodes = "nodes",
                      dev = ".tsv") {
     mandatory.columns <- c("site", "decor", "a", "b", "xa", "ya", "xb", "yb")
     eds.shp.coords <- data.frame(xa = numeric(0), ya = numeric(0),
                                  xb = numeric(0), yb = numeric(0))
-    eds.file <- paste0(doss, "/", edges, dev)
-    nds.file <- paste0(doss, "/", nodes, dev)
+    eds.file <- paste0(dir, "/", edges, dev)
+    nds.file <- paste0(dir, "/", nodes, dev)
     if (file.exists(eds.file) & file.exists(nds.file)) {
         if (dev == ".tsv" | dev == ".csv") {
             # choice: tsv or csv read nodes to get coordinates
-            nds.df <- read_nds(doss = doss, site = site, decor = decor,
+            nds.df <- read_nds(dir = dir, site = site, decor = decor,
                                nodes = nodes, dev = dev)
             sep=c(.tsv = "\t", .csv = ";")
             eds.df <- utils::read.table(file = eds.file, sep = sep[dev], header = TRUE,
@@ -28,7 +28,7 @@ read_eds <- function(site, decor, doss = getwd(),
         }
         if (dev == ".shp") {
             # choice: shapefile
-            eds.shp <- rgdal::readOGR(dsn = doss, layer = edges, verbose = FALSE)
+            eds.shp <- rgdal::readOGR(dsn = dir, layer = edges, verbose = FALSE)
             eds.shp <- eds.shp[eds.shp@data$site  == site &
                                eds.shp@data$decor == decor, ]
             # extract edges coordinates
